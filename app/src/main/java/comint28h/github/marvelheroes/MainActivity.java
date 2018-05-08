2c70fc7bc1a275bbec75a228f00ca3f70d23a2d9
Elena Listratova <int28h@protonmail.com>
2018-05-08 21:35:04 +0400

    Здесь что-то происходит

---
+++ app/src/main/java/comint28h/github/marvelheroes/MainActivity.java
@@ -0,0 +1,14 @@
+package comint28h.github.marvelheroes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
