1a5c9da05a2d09a65cb1f7a84618270edf882051
Elena Listratova <int28h@protonmail.com>
2018-06-25 23:48:42 +0400

    get parameters

--- app/src/main/java/comint28h/github/marvelheroes/APIService.java
+++ app/src/main/java/comint28h/github/marvelheroes/APIService.java
@@ -6,6 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIService {
-    @GET("v1/public/characters")
-    Call<List<Hero>> getHeroesList();
+    @GET("v1/public/characters)
+    Call<List<Hero>> getHeroesList(@Query ("ts") "1", @Query ("apikey") "c91320537ce6d3be8c6ac1b579d52e3e");
 }
