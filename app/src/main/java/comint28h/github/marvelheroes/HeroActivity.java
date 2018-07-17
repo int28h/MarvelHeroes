cb69b6bdbad9968d5bd08be450ea6e5b5ebad6b1
Elena Listratova <int28h@protonmail.com>
2018-07-17 06:04:05 +0400

    Swipe-to-Refresh for the second screen (HeroActivity).

--- app/src/main/java/comint28h/github/marvelheroes/HeroActivity.java
+++ app/src/main/java/comint28h/github/marvelheroes/HeroActivity.java
@@ -1,6 +1,7 @@
 package comint28h.github.marvelheroes;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,6 +11,8 @@ import com.squareup.picasso.Picasso;
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 public class HeroActivity extends AppCompatActivity {
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     ImageView thumbnail;
     TextView name;
     TextView description;
@@ -20,29 +23,47 @@ public class HeroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hero_activity);
-        
+
+        swipeRefreshLayout = findViewById(R.id.swipe_to_refresh_info);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                clearViewsValues();
+                loadHeroInfo();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        loadViews();
         loadHeroInfo();
+    }
+
+    private void loadViews(){
+        this.name = findViewById(R.id.name);
+        this.thumbnail = findViewById(R.id.portrait);
+        this.description = findViewById(R.id.description);
+        this.comics = findViewById(R.id.comics);
+        this.series = findViewById(R.id.series);
+    }
+
+    private void clearViewsValues(){
+        this.name.setText(null);
+        this.thumbnail.setImageURI(null);
+        this.description.setText(null);
+        this.comics.setText(null);
+        this.series.setText(null);
     }
 
     private void loadHeroInfo() {
         Bundle bundle = getIntent().getExtras();
 
-        this.name = findViewById(R.id.name);
         this.name.setText(bundle.getString("name"));
-
-        this.thumbnail = findViewById(R.id.portrait);
         Picasso.with(getApplicationContext())
                 .load(bundle.getString("thumbnail"))
                 .transform(new RoundedCornersTransformation(10, 10))
                 .into(this.thumbnail);
-
-        this.description = findViewById(R.id.description);
         this.description.setText(bundle.getString("description"));
-
-        this.comics = findViewById(R.id.comics);
         this.comics.setText(bundle.getString("comics"));
-
-        this.series = findViewById(R.id.series);
         this.series.setText(bundle.getString("series"));
     }
 }
